3d9d70f798da96c8561d7b50647b47de3307d71d
Lars Holmberg <lars.holmberg@svenskaspel.se>
2026-09-15 10:13:51 +0200

    remove release.sh and move its code into bump.sh

--- bump.sh
+++ bump.sh
@@ -1,2 +1,12 @@
 #!/usr/bin/env bash
-git tag $(git tag --sort=-creatordate | head -n 1 | awk -F. '{OFS="."; $NF+=1; print $0}') && git push origin --tags
+# Creates a new github release
+set -e
+
+latest=$(git tag --sort=-version:refname | head -n1)
+version=${latest}
+IFS=. read -r major minor patch <<< "$version"
+next="$major.$minor.$((patch + 1))"
+
+git tag "$next"
+git push origin "$next"
+gh release create "$next" --generate-notes
